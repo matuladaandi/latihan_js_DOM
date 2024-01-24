bea9934678caf9cf759b8a7f11d332aa2ce2d487
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-01-24 20:52:40 +0800

    mengerakan mouse dapat meubah warna secara acak

--- game-warna/js/script.js
+++ game-warna/js/script.js
@@ -25,3 +25,54 @@ btnRandomColor.addEventListener("click", function () {
   const b = Math.round(Math.random() * 255 + 1);
   document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 });
+
+// tangkap element
+const sliderRed = document.querySelector("input[name=sliderRed]");
+const sliderGreen = document.querySelector("input[name=sliderGreen]");
+const sliderBlue = document.querySelector("input[name=sliderBlue]");
+
+// event ketika slider di geser-geser
+sliderRed.addEventListener("input", function () {
+  const r = sliderRed.value;
+
+  const g = sliderGreen.value;
+
+  const b = sliderBlue.value;
+
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+
+  // mengambil nilainya
+  //    console.info(sliderRed.value);
+});
+sliderGreen.addEventListener("input", function () {
+  const r = sliderRed.value;
+
+  const g = sliderGreen.value;
+
+  const b = sliderBlue.value;
+
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+});
+sliderBlue.addEventListener("input", function () {
+  const r = sliderRed.value;
+
+  const g = sliderGreen.value;
+
+  const b = sliderBlue.value;
+
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+});
+
+// ketika mouser bergerak di body
+document.body.addEventListener("mousemove", function (event) {
+  // posisi mouse
+  //   console.info(event.clientY);
+
+  // ukuran browser
+  // console.info(window.innerWidth);
+
+  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+
+  document.body.style.backgroundColor = "rgb(" + xPos + ", " + yPos + ",0)";
+});
